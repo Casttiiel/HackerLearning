d6f4ee6858e143e3d472b32357ce8771048c737e
Algaor <algaor07@gmail.com>
2022-01-21 16:25:00 +0100

    SegundaSesion

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   hero : Hero = {
     id: 1,
@@ -13,7 +14,18 @@ export class HeroesComponent implements OnInit {
   };
   constructor() { }
 
+  heroes = HEROES;
+
   ngOnInit(): void {
   }
+
+
+  
+  selectedHero?: Hero;
+
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
 
 }
