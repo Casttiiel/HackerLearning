574e547c3058fc097897fcecf660fece146f8d21
Algaor <algaor07@gmail.com>
2022-01-28 19:52:34 +0100

    Sesion6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },// path por defecto que nos redirige a dashboard
+  { path: 'home', component: HeroesComponent },//cuando el path lleve a home, dará al component : heroescomponent.
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
