f1441450a0089f29145589d868c99b3d13815a50
Algaor <algaor07@gmail.com>
2022-02-01 17:12:33 +0100

    Sesion8

--- src/app/hero-table/hero-table.component.ts
+++ src/app/hero-table/hero-table.component.ts
@@ -12,10 +12,25 @@ export class HeroTableComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
   heroes?: Hero[];
+  heromaxgold?: Hero;
 
   ngOnInit(): void {
-    this.heroService.getHeroes().subscribe(data => this.heroes = data);
+    this.heroService.getHeroes().subscribe(data => {
+      this.heroes = data;
+      this.top5gold(data); //Lo añadimos aqui para que nos ejecute la funcion despues de recibir los datos de los heroes. Si lo ponemos fuera lo haria a la vez y no funcionaria.
+    });
     
   }
 
+  top5gold(lista:Hero[]): void{
+  var maxGold=0;
+    for(var id=0;id<lista.length;id++){
+        
+        if(lista[id].gold>maxGold){
+
+          this.heromaxgold= lista[id];
+          maxGold= lista[id].gold;
+        }
+    }
+  }
 }
