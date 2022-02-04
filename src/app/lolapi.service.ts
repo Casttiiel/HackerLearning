12a25db8ffe6aa96bbbb9b93caa3ddb230299db7
Dani <casttiiel@gmail.com>
2022-02-04 09:30:53 +0100

    [LOLAPI]: Better usage of session storage

--- src/app/lolapi.service.ts
+++ src/app/lolapi.service.ts
@@ -12,17 +12,17 @@ export class LolAPIService {
   constructor(private http: HttpClient) { }
 
   server: string = "https://euw1.api.riotgames.com";
-  key: string = "192d";
+  key: string = "RGAPI-54533add-3fe7-4e89-b103-cb4ac5a8192d";
 
   //TODO Moverlo a un servicio helper
   getData(url: string, sessionStorageKey: string) :Observable<any> {
     //si tengo en el session storage los datos, la devuelvo, sino, hago la llamada a la API y la devuelvo
-    if(sessionStorage[sessionStorageKey]){
-      return of(JSON.parse(sessionStorage[sessionStorageKey]));
+    if(sessionStorage.getItem(sessionStorageKey)){
+      return of(JSON.parse(sessionStorage.getItem(sessionStorageKey) || ""));
     }else{
       return this.http.get<any>(url).pipe(
         tap(data => { // Esto es otro subscribe
-          sessionStorage[sessionStorageKey] = JSON.stringify(data);
+          sessionStorage.setItem(sessionStorageKey, JSON.stringify(data));
         })
       );
     }
