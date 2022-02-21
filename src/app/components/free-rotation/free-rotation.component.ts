1121bf76dcc1f16648047cd27476636acacd6e17
Dani <casttiiel@gmail.com>
2022-02-21 21:37:12 +0100

    Create variables inside functions

--- src/app/components/free-rotation/free-rotation.component.ts
+++ src/app/components/free-rotation/free-rotation.component.ts
@@ -11,40 +11,33 @@ export class FreeRotationComponent implements OnInit {
 
   constructor(private lolApiService: LolAPIService) { }
 
-  data:ChampionData[]=[];
-  key:String="";
-  finalChampions:ChampionData[]=[];
-  lowFinalChampions:ChampionData[]=[];
+  finalChampions : ChampionData[] = [];
+  lowFinalChampions : ChampionData[] = [];
 
   ngOnInit(): void {
     this.lolApiService.clearData();
     
     this.lolApiService.getChampions().subscribe(datos=>{
-      //PILLAMOS LA STRING DONDE ESTA GUARDADO EL ID DEL CAMPEON PARA COMPARARLO CON EL NUMERO DE ID DE LOS CAMPEONES GRATUITOS.
-      this.data=Object.values(datos.data);
+      var data : ChampionData[] = Object.values(datos.data);
 
-      this.lolApiService.getChampionRotation().subscribe(datos2=>{
-       
-
-        for(var i=0;i<this.data.length;i++){
-          
-          this.key=this.data[i].key;
-          
-          for(var j=0;j<Object.values(datos2.freeChampionIds).length;j++){
-            
-            if(Number(this.key)===Object.values(datos2.freeChampionIds)[j]){
-              
-              this.finalChampions.push(this.data[i]);
+      this.lolApiService.getChampionRotation().subscribe(datos2 => {
+        var key : String = "";
+        for(var i = 0; i < data.length; i++){
+          key = data[i].key;
+          for(var j=0; j < Object.values(datos2.freeChampionIds).length; j++){
+            if(Number(key) === Object.values(datos2.freeChampionIds)[j]){
+              this.finalChampions.push(data[i]);
             }
           }
-          for(var k=0;k<Object.values(datos2.freeChampionIdsForNewPlayers).length;k++){
-            if(Number(this.key)===Object.values(datos2.freeChampionIdsForNewPlayers)[k]){
-              
-              this.lowFinalChampions.push(this.data[i]);
+          for(var k = 0; k < Object.values(datos2.freeChampionIdsForNewPlayers).length; k++){
+            if(Number(key) === Object.values(datos2.freeChampionIdsForNewPlayers)[k]){
+              this.lowFinalChampions.push(data[i]);
             }
-          }  
+          }
         }
-      })  
-    })
+      });
+    });
+
+
   }
 }
